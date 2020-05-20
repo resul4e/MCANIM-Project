f58f6ee27dffc84053dec8447873b0abe2828761
Resul Çelik <resul_celik@hotmail.com>
2020-05-20 19:00:26 +0200

    Added way to retrieve channels.

--- Source/AnimationClip.cpp
+++ Source/AnimationClip.cpp
@@ -12,3 +12,10 @@ void AnimationClip::AddChannel(std::shared_ptr<Channel> _channel)
 {
 	m_channels.insert(m_channels.end(), { _channel->GetJointName(), _channel });
 }
+
+std::shared_ptr<Channel> AnimationClip::GetChannel(std::string _name) const
+{
+	assert(m_channels.find(_name) == m_channels.end());
+	
+	return m_channels[_name];
+}
