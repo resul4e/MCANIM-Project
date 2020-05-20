5c438cd45dcff0cfc76a9e34d39ec0a5ff0155b3
Resul Çelik <resul_celik@hotmail.com>
2020-05-20 19:00:11 +0200

    Adding comments to AnimationClip and KeyFrame,

--- Source/AnimationClip.h
+++ Source/AnimationClip.h
@@ -6,13 +6,32 @@
 
 class Channel;
 
+/**
+ * \brief A single animation. Holds a list of keyframes for each bone.
+ */
 class AnimationClip
 {
 public:
+	/**
+	 * \brief Create a new animation.
+	 * \param _animationName The name of the animation
+	 * \param _fps How many frames are in a second.
+	 */
 	AnimationClip(std::string _animationName, float _fps);
 	~AnimationClip() = default;
 
+	/**
+	 * \brief Adds a new channel. Each channel stores a list of all keyframes for a single joint.
+	 * \param _channel The new channel to be added.
+	 */
 	void AddChannel(std::shared_ptr<Channel> _channel);
+
+	/**
+	 * \brief Returns the channel that corresponds to the given name.
+	 * \param _name The name of the channel we want to get.
+	 * \return The channel with the name _name.
+	 */
+	std::shared_ptr<Channel> GetChannel(std::string _name) const;
 
 private:
 	std::string m_animationName;
--- Source/KeyFrame.h
+++ Source/KeyFrame.h
@@ -2,12 +2,23 @@
 #include <glm/vec3.hpp>
 #include <glm/gtc/quaternion.hpp>
 
+/**
+ * \brief A checkpoint that stores the transformation for a single bone at a certain time.
+ * These are then interpolated with the previous or latter KeyFrame to get the value at any time.
+ */
 class KeyFrame
 {
 public:
+	/**
+	 * \brief 
+	 * \param _time The time of this KeyFrame in frametime.
+	 * \param _position The position of the joint at this KeyFrame.
+	 * \param _rotation The rotation of the joint at this KeyFrame.
+	 * \param _scale The scale of the joint at this KeyFrame.
+	 */
 	KeyFrame(float _time, glm::vec3 _position, glm::dquat _rotation, glm::vec3 _scale);
 	~KeyFrame() = default;
-
+	
 private:
 	float m_time;
 	glm::vec3 m_position;
