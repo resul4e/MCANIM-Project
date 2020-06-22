b6ca6c95fe8eb4099f2672fe80828b16ad1120d6
Julian Thijssen <Nimthora@gmail.com>
2020-06-22 12:22:20 +0200

    Clean up renderer

--- Source/Renderer.cpp
+++ Source/Renderer.cpp
@@ -5,7 +5,6 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
-#include <glm/gtx/euler_angles.hpp>
 
 #include <glad/glad.h>
 
@@ -13,21 +12,32 @@ bool Options::RenderModel = true;
 bool Options::RenderTextures = true;
 
 Renderer::Renderer() :
-	renderRig(true),
-	skyTexture(0)
+	m_renderRig(true),
+	m_modelShader(nullptr),
+	m_planeShader(nullptr),
+	m_rigShader(nullptr),
+	m_skyShader(nullptr),
+	m_skyTexture(nullptr),
+	m_dummyVao(0),
+	m_armatureVao(0),
+	m_armaturePbo(0),
+	m_renderWidth(0),
+	m_renderHeight(0)
 {
 
 }
 
 void Renderer::Initialize(std::filesystem::path _assetPath)
 {
+	// Set the background color to a light grey
 	glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
 
-	shader = new Shader(_assetPath.string() + "/Shaders/model.shader");
-	planeShader = new Shader(_assetPath.string() + "/Shaders/plane.shader");
-	rigShader = new Shader(_assetPath.string() + "/Shaders/rig.shader");
-	skyShader = new Shader(_assetPath.string() + "/Shaders/sky.shader");
+	// Load all the shaders from file
+	m_modelShader = new Shader(_assetPath.string() + "/Shaders/model.shader");
+	m_planeShader = new Shader(_assetPath.string() + "/Shaders/plane.shader");
+	m_rigShader = new Shader(_assetPath.string() + "/Shaders/rig.shader");
+	m_skyShader = new Shader(_assetPath.string() + "/Shaders/sky.shader");
 
 	// Generate a VAO and VBO for the armature drawing
 	glGenVertexArrays(1, &m_armatureVao);
@@ -42,141 +52,150 @@ void Renderer::Initialize(std::filesystem::path _assetPath)
 	glGenVertexArrays(1, &m_dummyVao);
 }
 
-void Renderer::Resize(Scene& scene, unsigned int width, unsigned int height)
+void Renderer::Resize(Scene& _scene, unsigned int _width, unsigned int _height)
 {
-	scene.GetCamera().SetAspectRatio((float) width / height);
-	renderWidth = width;
-	renderHeight = height;
+	// Adjust camera aspect ratio
+	_scene.GetCamera().SetAspectRatio((float) _width / _height);
+	// Store width and height for render viewport
+	m_renderWidth = _width;
+	m_renderHeight = _height;
 }
 
-void Renderer::Update(Scene& scene)
+void Renderer::Update(Scene& _scene)
 {
+	// Clear the background and set the viewport to the window size
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glViewport(0, 0, renderWidth, renderHeight);
+	glViewport(0, 0, m_renderWidth, m_renderHeight);
 
-	//RenderSky(scene);
-	RenderGroundPlane(scene);
-	RenderModel(scene);
-	RenderRig(scene);
+	// Render the checkerboard floor
+	float planeSize = _scene.GetModel().m_bounds.getMaxDimension() * 8;
+	float planeHeight = _scene.GetModel().m_bounds.getMin().y;
+	RenderGroundPlane(_scene.GetGroundPlane(), _scene.GetCamera(), planeSize, planeHeight);
+
+	// Render the model geometry
+	RenderModel(_scene);
+
+	// Render the model armature
+	RenderRig(_scene);
 }
 
-void Renderer::SetBackgroundTexture(std::shared_ptr<Texture> texture)
+void Renderer::SetBackgroundTexture(std::shared_ptr<Texture> _texture)
 {
-	skyTexture = texture;
+	m_skyTexture = _texture;
 }
 
 void Renderer::ToggleRigRendering()
 {
-	renderRig = !renderRig;
+	m_renderRig = !m_renderRig;
 }
 
-void Renderer::RenderSky(Scene& scene)
+void Renderer::RenderSky(Scene& _scene)
 {
-	if (skyTexture == nullptr) return;
+	if (m_skyTexture == nullptr) return;
 	
-	skyShader->Bind();
-	skyTexture->Bind(0);
+	m_skyShader->Bind();
 
 	glm::mat4 projMatrix(1);
-	scene.GetCamera().loadProjectionMatrix(projMatrix);
+	_scene.GetCamera().loadProjectionMatrix(projMatrix);
 
 	glm::mat4 yawMatrix = glm::rotate(0.0f, glm::vec3(0, 1, 0));
 	glm::mat4 pitchMatrix = glm::rotate(0.0f, glm::vec3(1, 0, 0));
 
 	glm::mat4 viewMatrix(1);
-	glm::vec3 modelCenter = scene.GetModel().m_bounds.getCenter();
-	scene.GetCamera().LookAt(viewMatrix, scene.GetCamera().position, modelCenter, glm::vec3(0, 1, 0));
+	glm::vec3 modelCenter = _scene.GetModel().m_bounds.getCenter();
+	_scene.GetCamera().LookAt(viewMatrix, _scene.GetCamera().position, modelCenter, glm::vec3(0, 1, 0));
 
+	// Find the camera basis vectors
 	glm::mat4 cameraBasis = glm::mat4(glm::mat3(viewMatrix));
 	cameraBasis[3][3] = 1;
-	//cameraBasis[2][2] = -cameraBasis[2][2];
-	//cameraBasis = yawMatrix * pitchMatrix * cameraBasis;
-	float pitch, yaw, z;
-	glm::extractEulerAngleXYZ(cameraBasis, pitch, yaw, z);
 
-	skyShader->SetUniform1i("tex", 0);
-	skyShader->SetVec2("persp", glm::vec2(1.0f / projMatrix[0][0], 1.0f / projMatrix[1][1]));
-	skyShader->SetMatrix4("cameraBasis", cameraBasis);
+	m_skyShader->SetUniform1i("tex", 0);
+	m_skyShader->SetVec2("persp", glm::vec2(1.0f / projMatrix[0][0], 1.0f / projMatrix[1][1]));
+	m_skyShader->SetMatrix4("cameraBasis", cameraBasis);
 	glBindVertexArray(m_dummyVao);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
-void Renderer::RenderGroundPlane(Scene& scene)
+void Renderer::RenderGroundPlane(Model& _plane, Camera& _camera, float _size, float _height)
 {
 	// Enable testing against z-buffer depth
 	glEnable(GL_DEPTH_TEST);
 
-	planeShader->Bind();
+	m_planeShader->Bind();
 
+	// Fill and upload PVM matrices
 	glm::mat4 projMatrix(1);
-	scene.GetCamera().loadProjectionMatrix(projMatrix);
-
 	glm::mat4 viewMatrix(1);
-	scene.GetCamera().LookAt(viewMatrix, scene.GetCamera().position, scene.GetCamera().center, glm::vec3(0, 1, 0));
-
 	glm::mat4 modelMatrix(1);
-	modelMatrix = glm::translate(modelMatrix, glm::vec3(0, scene.GetModel().m_bounds.getMin().y, 0));
-	float maxDimension = scene.GetModel().m_bounds.getMaxDimension();
-	modelMatrix = glm::scale(modelMatrix, glm::vec3(maxDimension*8, 1, maxDimension*8));
 
-	planeShader->SetMatrix4("projMatrix", projMatrix);
-	planeShader->SetMatrix4("viewMatrix", viewMatrix);
-	planeShader->SetMatrix4("modelMatrix", modelMatrix);
+	_camera.loadProjectionMatrix(projMatrix);
+	_camera.LookAt(viewMatrix, _camera.position, _camera.center, glm::vec3(0, 1, 0));
+	modelMatrix = glm::translate(modelMatrix, glm::vec3(0, _height, 0));
+	modelMatrix = glm::scale(modelMatrix, glm::vec3(_size, 1, _size));
 
-	for (Mesh& mesh : scene.GetGroundPlane().meshes)
+	m_planeShader->SetMatrix4("projMatrix", projMatrix);
+	m_planeShader->SetMatrix4("viewMatrix", viewMatrix);
+	m_planeShader->SetMatrix4("modelMatrix", modelMatrix);
+
+	// Render plane
+	for (Mesh& mesh : _plane.meshes)
 	{
 		glBindVertexArray(mesh.vao);
 		glDrawArrays(GL_TRIANGLES, 0, mesh.faces.size() * 3);
 	}
 
-	planeShader->UnBind();
+	m_planeShader->UnBind();
 }
-void Renderer::RenderModel(Scene& scene)
+
+void Renderer::RenderModel(Scene& _scene)
 {
 	if (!Options::RenderModel) return;
 
 	// Enable testing against z-buffer depth
 	glEnable(GL_DEPTH_TEST);
 
-	shader->Bind();
+	m_modelShader->Bind();
 
+	// Fill and upload PVM matrices
 	glm::mat4 projMatrix(1);
-	scene.GetCamera().loadProjectionMatrix(projMatrix);
-
 	glm::mat4 viewMatrix(1);
-	glm::vec3 modelCenter = scene.GetModel().m_bounds.getCenter();
-	scene.GetCamera().LookAt(viewMatrix, scene.GetCamera().position, modelCenter, glm::vec3(0, 1, 0));
-
 	glm::mat4 modelMatrix(1);
 
-	shader->SetMatrix4("projMatrix", projMatrix);
-	shader->SetMatrix4("viewMatrix", viewMatrix);
-	shader->SetMatrix4("modelMatrix", modelMatrix);
+	_scene.GetCamera().loadProjectionMatrix(projMatrix);
+	_scene.GetCamera().LookAt(viewMatrix, _scene.GetCamera().position, _scene.GetCamera().center, glm::vec3(0, 1, 0));
 
-	if (scene.GetTexture() != nullptr && Options::RenderTextures)
+	m_modelShader->SetMatrix4("projMatrix", projMatrix);
+	m_modelShader->SetMatrix4("viewMatrix", viewMatrix);
+	m_modelShader->SetMatrix4("modelMatrix", modelMatrix);
+
+	// Bind model textures
+	if (_scene.GetTexture() != nullptr && Options::RenderTextures)
 	{
-		scene.GetTexture()->Bind(0);
-		shader->SetUniform1i("u_Texture", 0);
-		shader->SetUniform1i("u_HasTexture", 1);
-		scene.GetSpecularMap()->Bind(2);
-		shader->SetUniform1i("u_specular", 2);
+		_scene.GetTexture()->Bind(0);
+		m_modelShader->SetUniform1i("u_Texture", 0);
+		m_modelShader->SetUniform1i("u_HasTexture", 1);
+		_scene.GetSpecularMap()->Bind(2);
+		m_modelShader->SetUniform1i("u_specular", 2);
 	}
 	else
 	{
-		shader->SetUniform1i("u_HasTexture", 0);
+		m_modelShader->SetUniform1i("u_HasTexture", 0);
 	}
 	
-	skyTexture->Bind(1);
-	shader->SetUniform1i("u_EnvTexture", 1);
-	shader->SetVec3("u_CamPos", scene.GetCamera().position);
+	// Bind environment texture
+	m_skyTexture->Bind(1);
+	m_modelShader->SetUniform1i("u_EnvTexture", 1);
+	// Set camera position
+	m_modelShader->SetVec3("u_CamPos", _scene.GetCamera().position);
 
-	for (Mesh& mesh : scene.GetModel().meshes)
+	// Render model
+	for (Mesh& mesh : _scene.GetModel().meshes)
 	{
 		glBindVertexArray(mesh.vao);
 		glDrawArrays(GL_TRIANGLES, 0, mesh.faces.size() * 3);
 	}
 
-	shader->UnBind();
+	m_modelShader->UnBind();
 }
 
 struct Line
@@ -185,44 +204,47 @@ struct Line
 	glm::vec4 end;
 };
 
-void ComputeArmature(std::vector<Line>& lines, const Joint& joint)
+// Compute the line segments that need to be drawn to represent the armature hierarchy
+void ComputeArmature(std::vector<Line>& _lines, const Joint& _joint)
 {
-	glm::mat4 transform = joint.GetGlobalTransform();
+	glm::mat4 transform = _joint.GetGlobalTransform();
 	glm::vec4 startPosition = transform * glm::vec4(0, 0, 0, 1);
 	
-	std::vector<std::shared_ptr<Joint>>& children = joint.GetChildren();
+	std::vector<std::shared_ptr<Joint>>& children = _joint.GetChildren();
 	for (auto child : children)
 	{
 		glm::mat4 childTransform = child->GetGlobalTransform();
 		glm::vec4 endPosition = childTransform * glm::vec4(0, 0, 0, 1);
-		lines.push_back(Line{ startPosition, endPosition });
+		_lines.push_back(Line{ startPosition, endPosition });
 
-		ComputeArmature(lines, *child);
+		ComputeArmature(_lines, *child);
 	}
 }
 
-void Renderer::RenderRig(Scene& scene)
+void Renderer::RenderRig(Scene& _scene)
 {
-	if (!renderRig) return;
+	if (!m_renderRig) return;
 
 	// Draw without considering z-buffer depth
 	glDisable(GL_DEPTH_TEST);
 
-	rigShader->Bind();
+	m_rigShader->Bind();
 
+	// Fill and upload PVM matrices
 	glm::mat4 projMatrix(1);
-	scene.GetCamera().loadProjectionMatrix(projMatrix);
-
 	glm::mat4 viewMatrix(1);
-	glm::vec3 modelCenter = scene.GetModel().m_bounds.getCenter();
-	scene.GetCamera().LookAt(viewMatrix, scene.GetCamera().position, modelCenter, glm::vec3(0, 1, 0));
+
+	_scene.GetCamera().loadProjectionMatrix(projMatrix);
+	_scene.GetCamera().LookAt(viewMatrix, _scene.GetCamera().position, _scene.GetCamera().center, glm::vec3(0, 1, 0));
 	
-	rigShader->SetMatrix4("projMatrix", projMatrix);
-	rigShader->SetMatrix4("viewMatrix", viewMatrix);
-	rigShader->SetVec3("u_Color", Options::RenderModel ? glm::vec3(0.5, 1, 1) : glm::vec3(0, 0, 0));
+	m_rigShader->SetMatrix4("projMatrix", projMatrix);
+	m_rigShader->SetMatrix4("viewMatrix", viewMatrix);
+
+	// Set armature rendering color
+	m_rigShader->SetVec3("u_Color", Options::RenderModel ? glm::vec3(0.5, 1, 1) : glm::vec3(0, 0, 0));
 
 	// Compute the armature lines
-	const Rig& rig = *scene.GetRig();
+	const Rig& rig = *_scene.GetRig();
 	std::vector<Line> bones;
 	ComputeArmature(bones, *rig.GetRootJoint());
 
@@ -233,5 +255,5 @@ void Renderer::RenderRig(Scene& scene)
 	// Ignore the root bone drawing
 	glDrawArrays(GL_LINES, 2, bones.size() * 2);
 
-	rigShader->UnBind();
+	m_rigShader->UnBind();
 }
--- Source/Renderer.h
+++ Source/Renderer.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include "Shader.h"
 #include "Camera.h"
 #include "Scene.h"
-#include "Shader.h"
 #include "Texture.h"
 
 #include <filesystem>
@@ -12,33 +12,88 @@ class Renderer
 {
 public:
 	Renderer();
+
+	/**
+	 * \brief Initializes the renderer by setting OpenGL state, loading the shaders
+	 * and generating appropriate GPU objects.
+	 * \param _assetPath The path that the assets are located at
+	 */
 	void Initialize(std::filesystem::path _assetPath);
-	void Resize(Scene& scene, unsigned int width, unsigned int height);
-	void Update(Scene& scene);
-	void SetBackgroundTexture(std::shared_ptr<Texture> texture);
+	
+	/**
+	 * \brief Resizes the area on which rendering will happen
+	 * \param _scene The scene containing the main camera for getting the aspect ratio
+	 * \param _width The width of the area we want to render to
+	 * \param _height The height of the area we want to render to
+	 */
+	void Resize(Scene& _scene, unsigned int _width, unsigned int _height);
+	
+	/**
+	 * \brief Performs all rendering (model, rig, groundplane)
+	 * \param _scene The scene containing the model, rig and groundplane
+	 */
+	void Update(Scene& _scene);
+	
+	/**
+	 * \brief Sets a background image to use for sky rendering and environment lighting
+	 * \param _texture The texture containing the cylindrical mapped environment image
+	 */
+	void SetBackgroundTexture(std::shared_ptr<Texture> _texture);
+	
+	/**
+	 * \brief Turn rendering of the rig on and off
+	 */
 	void ToggleRigRendering();
-	bool IsRenderingRig() { return renderRig; }
+	
+	/**
+	 * \brief Check if the rig is being rendered
+	 * \return true if the rig is rendering
+	 */
+	bool IsRenderingRig() { return m_renderRig; }
 
 private:
-	void RenderSky(Scene& scene);
-	void RenderGroundPlane(Scene& scene);
-	void RenderModel(Scene& scene);
-	void RenderRig(Scene& scene);
+	/**
+	 * \brief Renders the background environment image rather than a flat color
+	 * \param _scene The scene containing the model and camera
+	 */
+	void RenderSky(Scene& _scene);
 
-	bool renderRig;
+	/**
+	 * \brief Renders the ground checkerboard plane
+	 * \param _scene The scene containing the model and camera
+	 */
+	void RenderGroundPlane(Model& _plane, Camera& _camera, float _size, float _height);
+
+	/**
+	 * \brief Renders the model geometry
+	 * \param _scene The scene containing the model
+	 */
+	void RenderModel(Scene& _scene);
+
+	/**
+	 * \brief Renders all the bones of the model as a connected line hierarchy
+	 * \param _scene The scene containing the armature
+	 */
+	void RenderRig(Scene& _scene);
+
+	/** Whether to render the armature or not */
+	bool m_renderRig;
 	
-	Shader* shader;
-	Shader* planeShader;
-	Shader* rigShader;
-	Shader* skyShader;
-	std::shared_ptr<Texture> skyTexture;
-	std::shared_ptr<Model> m_bone;
+	// Shaders
+	Shader* m_modelShader;
+	Shader* m_planeShader;
+	Shader* m_rigShader;
+	Shader* m_skyShader;
 
+	// Environment texture
+	std::shared_ptr<Texture> m_skyTexture;
+
+	// Vertex array objects necessary for rendering
 	unsigned int m_dummyVao;
 	unsigned int m_armatureVao;
 	unsigned int m_armaturePbo;
 
 	// The screen size at which we should render
-	int renderWidth;
-	int renderHeight;
+	int m_renderWidth;
+	int m_renderHeight;
 };
